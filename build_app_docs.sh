f11a24131232fefc46408f2aac91313869976681
Ole-André Rodlie <ole.andre.rodlie@gmail.com>
2026-02-13 16:39:14 +0100

    Update build_app_docs.sh

--- build_app_docs.sh
+++ build_app_docs.sh
@@ -33,9 +33,6 @@ mkdir -p _layouts assets/backgrounds assets/documentation
 cp ${CWD}/_config_app.yml _config.yml
 cp ${CWD}/_layouts_app/* _layouts/
 cp ${CWD}/assets/style-doc.css assets/style.css
-cp ${CWD}/assets/logo.svg assets/
-cp ${CWD}/assets/backgrounds/friction-background-007.svg assets/backgrounds/
-cp ${CWD}/assets/documentation/question.svg assets/documentation/
 
 PAGES="
 animation_techniques.md
